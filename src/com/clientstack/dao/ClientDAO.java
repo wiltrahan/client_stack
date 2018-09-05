42ab9106cca5e99da9d08f4af21e76029ad4dcd2
wiltrahan <twilorip@gmail.com>
2018-09-05 17:23:34 -0400

    set up DAO

---
+++ src/com/clientstack/dao/ClientDAO.java
@@ -0,0 +1,9 @@
+package com.clientstack.dao;
+
+import java.util.List;
+
+import com.clientstack.entity.Client;
+
+public interface ClientDAO {
+	public List<Client> getClients();
+}
---
+++ src/com/clientstack/dao/ClientDAOImpl.java
@@ -0,0 +1,38 @@
+package com.clientstack.dao;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.clientstack.entity.Client;
+
+@Repository
+public class ClientDAOImpl implements ClientDAO {
+
+	// inject hibernate session factory
+	@Autowired
+	private SessionFactory sessionFactory;
+	
+	@Override
+	@Transactional
+	public List<Client> getClients() {
+		
+		// get the current hibernate session
+		Session currentSession = sessionFactory.getCurrentSession();
+		
+		// create a query
+		Query<Client> theQuery = currentSession.createQuery("from Client", Client.class);
+		
+		// execute query and get result list
+		List<Client> clients = theQuery.getResultList();
+		
+		//return the results 
+		return clients;
+	}
+
+}
