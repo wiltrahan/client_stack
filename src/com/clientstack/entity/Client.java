d335665d351e0e996fec3f93e42fc459ce290044
wiltrahan <twilorip@gmail.com>
2018-09-05 17:01:08 -0400

    add entity class

---
+++ src/com/clientstack/entity/Client.java
@@ -0,0 +1,94 @@
+package com.clientstack.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="client")
+public class Client {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id")
+	private int id;
+	
+	@Column(name="lastName")
+	private String lastName;
+	
+	@Column(name="firstName")
+	private String firstName;
+	
+	@Column(name="phone")
+	private String phone;
+	
+	@Column(name="email")
+	private String email;
+	
+	@Column(name="formula")
+	private String formula;
+	
+	public Client() {
+		
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getFormula() {
+		return formula;
+	}
+
+	public void setFormula(String formula) {
+		this.formula = formula;
+	}
+
+	@Override
+	public String toString() {
+		return "Client [id=" + id + ", lastName=" + lastName + ", firstName=" + firstName + ", phone=" + phone
+				+ ", email=" + email + ", formula=" + formula + "]";
+	}
+	
+	
+	
+}
