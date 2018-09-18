fa58513e4228fb7449f6e24d619a0d15c9f8501b
wiltrahan <twilorip@gmail.com>
2018-09-18 17:15:56 -0400

    cont fullcalendar integration

--- WebContent/resources/js/myjQuery.js
+++ WebContent/resources/js/myjQuery.js
@@ -18,6 +18,23 @@ $(document).ready(function() {
     });
     
     $('#calendar').fullCalendar({
-        
-      })
+    	
+    	events: [
+    	    {
+    	      title  : 'event1',
+    	      start  : '2018-09-26'
+    	    },
+    	    {
+    	      title  : 'event2',
+    	      start  : '2018-09-27',
+    	      end    : '2018-09-28'
+    	    },
+    	    {
+    	      title  : 'event3',
+    	      start  : '2018-09-29T12:30:00',
+    	      allDay : false // will make the time show
+    	    }
+    	  ]
+    	
+    });
 });
